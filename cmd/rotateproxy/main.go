6a19ad34d35eb3137b2de0ce91a9eaa25452db91
Akkuman <akkumans@qq.com>
2024-01-23 15:54:06 +0800

    fix: dont check url by default

--- cmd/rotateproxy/main.go
+++ cmd/rotateproxy/main.go
@@ -34,10 +34,10 @@ func init() {
 	flag.StringVar(&token, "token", "", "token")
 	flag.StringVar(&proxy, "proxy", "", "proxy")
 	flag.StringVar(&rule, "rule", fmt.Sprintf(`protocol=="socks5" && "Version:5 Method:No Authentication(0x00)" && after="%s" && country="CN"`, time.Now().AddDate(0, -3, 0).Format(time.DateOnly)), "search rule")
-	flag.StringVar(&checkURL, "check", `https://www.google.com`, "check url")
-	flag.StringVar(&checkURLwords, "checkWords", `Copyright The Closure Library Authors`, "words in check url")
+	flag.StringVar(&checkURL, "check", ``, "check url")
+	flag.StringVar(&checkURLwords, "checkWords", ``, "words in check url")
 	flag.IntVar(&baseCfg.IPRegionFlag, "region", 0, "0: all 1: cannot bypass gfw 2: bypass gfw")
-	flag.IntVar(&baseCfg.SelectStrategy, "strategy", 3, "0: random, 1: Select the one with the shortest timeout, 2: Select the two with the shortest timeout, ...")
+	flag.IntVar(&baseCfg.SelectStrategy, "strategy", 1, "0: random, 1: Select the one with the shortest timeout")
 	flag.IntVar(&pageCount, "page", 5, "the page count you want to crawl")
 	flag.Parse()
 
